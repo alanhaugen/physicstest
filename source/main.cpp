48c3c57e384d902060ec1bd4124a508cf5518a0c
Alan <alanhaugen@gmail.com>
2023-05-20 18:47:53 +0200

    Made prettier colours and made small cube fall
    The bigger ground is static, small cube falls from the sky

--- source/main.cpp
+++ source/main.cpp
@@ -33,15 +33,16 @@ Demo::~Demo()
 
 void Demo::Init()
 {
+    // https://www.schemecolor.com/soft-and-cute.php
     ground = new Actor();
-    ground->Add(new Cube(0,-10,0,50,50,50));
-    ground->Uniform("colour", glm::vec4(0.1, 0.9, 0.9, 1.0));
+    ground->Add(new Cube(0,-2,0,2,2,2));
+    ground->Uniform("colour", glm::vec4(0.607, 0.721, 0.929, 1.0)); // cute pale cerulean
 
     cube = new Actor();
-    cube->Add(new Cube(0,0,0, 10,10,10));
-    cube->Uniform("colour", glm::vec4(0.9, 0.9, 0.0, 1.0));
+    cube->Add(new Cube(0,2,0, 1,1,1));
+    cube->Uniform("colour", glm::vec4(0.996, 0.776, 0.875, 1.0)); // cute classic rose
 
-    camera = new Camera(0,0,100);
+    camera = new Camera(0,0,10);
 
     fpsCamera = new FPSCamera(camera);
     fpsCounter = new FPSCounter();
@@ -60,10 +61,18 @@ void Demo::Update()
     ground->Uniform("u_cameraPosition", static_cast<glm::vec3>(camera->position));
     cube->Uniform("u_lightPosition", static_cast<glm::vec3>(lightPos));
     cube->Uniform("u_cameraPosition", static_cast<glm::vec3>(camera->position));
+
+    float gravity = 0.098f;
+    cube->matrix.Translate(glm::vec3(0.0f, -gravity, 0.0f));
 }
 
 void Demo::UpdateAfterPhysics()
 {
+    if (physics->Collide(cube->collisionBox)) // Collision
+    {
+        float gravity = 0.098f;
+        cube->matrix.Translate(glm::vec3(0.0f, gravity, 0.0f));
+    }
 }
 
 int main(int argc, char **argv)
